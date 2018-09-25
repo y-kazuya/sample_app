246d3a28b8eecfe4a9316a157b15ee24e4ab7614
y-kazuya <y.kazuya.0124@au.com>
2018-09-25 17:09:48 +0900

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,13 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do     #users/user_id/followingとかになる       memberじゃなくて collectionやったらidで区別しない
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]#user認証（リンクを踏むだけやからeditで！）
   resources :password_resets,     only: [:new, :create, :edit, :update]#PW再設定
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
